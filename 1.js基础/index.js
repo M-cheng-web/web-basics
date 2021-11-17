0861d92cb062baf03af1624f869af220a9405a2f
M-cheng-web <2604856589@qq.com>
2021-11-17 10:52:56 +0800

    feat: js -> 原型 & 变量提升

--- 1.js基础/index.js
+++
@@ -1,9 +0,0 @@
-var arr = [{ old: 'old' }, ['old']];
-
-var new_arr = arr.slice();
-
-arr[0].old = 'new';
-arr[1][0] = 'new';
-
-console.log(arr) // [{old: 'new'}, ['new']]
-console.log(new_arr) // [{old: 'new'}, ['new']]
